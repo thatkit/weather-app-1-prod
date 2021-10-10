b5bf98e10dbabf8eda3eab7d3b5c4cf46d36c97f
Nikita <emper137137@gmail.com>
2021-10-11 08:45:19 +1000

    a problem - how to use express.static with reacts public

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
@@ -6,5 +7,15 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
-    console.log(process.env.OPEN_WEATHER_API_KEY);
+});
+
+app.use(express.static('public'));
+
+app.get('/weather/:city', async (req, res) => {
+    const apiKEY = process.env.OPEN_WEATHER_API_KEY;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${apiKEY}`;
+
+    const response = await fetch(apiURL);
+    const json = await response.json();
+    console.log(json);
 });
