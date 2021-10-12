504108c99741919ca90e4c08db792ee9b9c53a6c
Nikita <emper137137@gmail.com>
2021-10-13 08:43:41 +1000

    trying out reactstrap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Jumbotron, Button } from 'reactstrap';
 import db from '../../db.json'; // for develompent only
 
-export const App = () => {
+export const App = (props) => {
     const [weather, setWeather] = useState({
         main: {
             temp: 0
@@ -33,14 +34,25 @@ export const App = () => {
     }, []);
     
     return (
+        // <div>
+        //     <h1>Weather App</h1>
+        //     <h2>Build with React.js, Express.js with Open Weather API</h2>
+        //     <div>
+        //         <p>
+        //             {weather.main.temp}
+        //         </p>
+        //     </div>
+        // </div>
         <div>
-            <h1>Weather App</h1>
-            <h2>Build with React.js, Express.js with Open Weather API</h2>
-            <div>
-                <p>
-                    {weather.main.temp}
+            <Jumbotron tag="tag-name" fluid={true}>
+                <h1 className="display-3">Weather for your coordinates!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                    <Button color="primary">Learn More</Button>
                 </p>
-            </div>
+            </Jumbotron>
         </div>
     )
 }
