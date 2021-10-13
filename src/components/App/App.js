0a3c6ca5b1ac2a046f7f53706a904bddf556a8e0
Nikita <emper137137@gmail.com>
2021-10-13 20:28:27 +1000

    basic reactstrap template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Jumbotron, Button } from 'reactstrap';
 import db from '../../db.json'; // for develompent only
+// change coords from api 
 
 export const App = (props) => {
     const [weather, setWeather] = useState({
+        coord: {
+            lon: 0,
+            lat: 0
+        },
         main: {
             temp: 0
-        }
+        },
+        wind: {
+            speed: 0,
+            deg: 0
+        },
+        name: ''
     });
     
     const getData = async () => {
@@ -43,16 +54,23 @@ export const App = (props) => {
         //         </p>
         //     </div>
         // </div>
-        <div>
-            <Jumbotron tag="tag-name" fluid={true}>
-                <h1 className="display-3">Weather for your coordinates!</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-2" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <Container>
+            <Jumbotron>
+                <h1 className="display-2 mt-4 mb-4">Weather for your coordinates!</h1>
                 <p className="lead">
-                    <Button color="primary">Learn More</Button>
+                    Your place: {weather.name}
                 </p>
+                <p className="lead">
+                    Your coordinates: {weather.coord.lon}&deg;, {weather.coord.lat}&deg;
+                </p>
+                <hr className="my-4" />
+                <p>
+                    here is the place for mini cards for each weather attribute with a small animated image
+                    
+                    {weather.wind.speed}, {weather.main.temp}, etc...
+                </p>
+                <Button color="secondary">Learn more</Button>
             </Jumbotron>
-        </div>
-    )
+        </Container>
+      )
 }
