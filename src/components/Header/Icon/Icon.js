770f22c02ecd9390ff54db630bc302c71b109bd4
Nikita <emper137137@gmail.com>
2021-10-17 12:20:14 +1000

    placing an svg image into Icon component - 50% done

--- src/components/Header/Icon/Icon.js
+++ src/components/Header/Icon/Icon.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import weatherIcon from './svg/weather.svg'
 
 export const Icon = (props) => {
+
+    console.log(props.description, props.icon)
+
+    const placeSVG = async elId => {
+        
+        // First, get the svg text and insert it into the DOM
+        const res = await fetch(weatherIcon);
+        const text = await res.text();
+        document.getElementById('icon').insertAdjacentHTML("afterbegin", text);
+        
+        // Second, assign the chosen element to a variable
+        const svgEl = document.querySelector(`svg#svg-icon > g#${elId}`);
+        
+        // Third, hide all other elements and reveal the chosen one
+        document.querySelectorAll('svg#svg-icon > g').forEach(el => el.style.display = 'none');
+        svgEl.style.display = 'block';
+
+    }
+
+    placeSVG('thunder');
+
     return (
-        <img
-            alt={'weather icon' + props.description}
-            src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`}
-        />
+        <div id="icon">
+        </div>
     )
 }
--- src/components/Tiles/Tile/Tile.js
+++ src/components/Tiles/Tile/Tile.js
@@ -4,15 +4,15 @@ import { Row, Col, Card, CardTitle, CardText } from 'reactstrap';
 
 export const Tile = (props) => {
     return (
-    <Card body outline color="secondary" className="mt-2 mb-3">
-        <Row>
-            <Col xs="8" sm="12">
-                <CardTitle tag="h5">{props.heading}</CardTitle>
-            </Col>
-            <Col xs="4" sm="12">
-                <CardText>{props.value}</CardText>
-            </Col>
-        </Row>
-    </Card>
+        <Card body outline color="secondary" className="mt-2 mb-3">
+            <Row>
+                <Col xs="8" sm="12">
+                    <CardTitle tag="h5">{props.heading}</CardTitle>
+                </Col>
+                <Col xs="4" sm="12">
+                    <CardText>{props.value}</CardText>
+                </Col>
+            </Row>
+        </Card>
     );
 }
