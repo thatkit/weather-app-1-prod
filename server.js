a1be7e55b4e8c92e75cae794172c3a5d3f0cc904
Nikita <emper137137@gmail.com>
2021-10-19 19:39:49 +1000

    https server for development

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+// const fs = require('fs');
+// const https = require('https');
+// const path = require('path');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+// const app = express();
+const app = require('https-localhost')();
 
+const port = process.env.PORT || 5000;
+// const httpsOptions = {
+//     cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt')),
+//     key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key')),
+// }
+
+// https.createServer(httpsOptions, app).listen(port, () => console.log(`Listening at http://localhost:${port}`));
+        
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 
 app.use(express.static('./build'));
